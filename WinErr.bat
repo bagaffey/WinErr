caa0522f689bd7f8ca1cd85c65e6a6ded0540d6e
Barry Gaffey <bagaffey@users.noreply.github.com>
2025-08-29 12:56:40 -0400

    fix: foreground color and background color cannot be the same

--- WinErr.bat
+++ WinErr.bat
@@ -1,12 +1,15 @@
 @ECHO OFF
-REM SPASS
 SETLOCAL EnableDelayedExpansion
+
 SET PTR=0
 SET BGC=0
 SET FGC=1
+
 :FA
-SET /A BGC=!RANDOM!%%16
-SET /A FGC=!RANDOM!%%16
+SET /A BGC=!RANDOM! %% 16, FGC=!RANDOM! %% 16
+
+IF !BGC! EQU !FGC! SET /A FGC = (FGC + 8) %% 16
+
 IF /I %FGC% EQU 10 (SET FGC=A)
 IF /I %FGC% EQU 11 (SET FGC=B)
 IF /I %FGC% EQU 12 (SET FGC=C)
@@ -19,12 +22,17 @@ IF /I %BGC% EQU 12 (SET BGC=C)
 IF /I %BGC% EQU 13 (SET BGC=D)
 IF /I %BGC% EQU 14 (SET BGC=E)
 IF /I %BGC% EQU 15 (SET BGC=F)
+
 COLOR %BGC%%FGC%
+
 ECHO -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 NET HELPMSG %PTR%
-REM Causes the system beep
+REM Causes the system beep (BEL character)
 ECHO 
+
 SET /A PTR+=1
+
 GOTO FA
+
 ENDLOCAL
 REM END OF FILE
